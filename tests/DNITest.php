450ae2906997023f2fb61a6984fc767b0395e535
Usuario user <gp.herrera@usp.ceu.es>
2022-11-30 17:41:50 +0100

    Test 3

--- tests/DNITest.php
+++ tests/DNITest.php
@@ -22,5 +22,10 @@ Class DNITest extends TestCase{
             assertEquals("La longitud del DNI no es válida", $e->getMessage(), "No se ha lanzado la excepción correcta");
         }
     }
+
+    public function testOchoCaracteresLetra():void{
+        $dni = new DNI();
+        assertTrue($dni->comprobarValidez("00000001R"), "La secuencia del DNI no es válida");
+    }
 }
 ?>
