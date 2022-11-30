f20342a39e01edf96b30fdb4c407942298039899
Usuario user <gp.herrera@usp.ceu.es>
2022-11-30 17:58:05 +0100

    Test 3 ok

--- src/DNI.php
+++ src/DNI.php
@@ -6,7 +6,16 @@ use Exception;
 
 Class DNI{
     public function comprobarValidez(string $dni):bool{
-        if (strlen($dni)==9) return true;
+        if (strlen($dni)==9){
+            $caracteres = str_split($dni);
+            $condiciones=0;
+            
+            foreach ($caracteres as $clave => $valor){    
+                if ($clave <= 7 && is_numeric($valor)) $condiciones+=1;
+                if ($clave == 8 && !is_numeric($valor)) $condiciones+=1;
+            }
+            if ($condiciones == 9) return true;
+        }
         else throw new Exception("La longitud del DNI no es válida");
     }
 }
