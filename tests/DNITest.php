cf4f682c64bc11b1dacb161d36e9b50ee2c25aff
Usuario user <gp.herrera@usp.ceu.es>
2022-11-30 17:37:40 +0100

    Test 1 y 2 modificados

--- tests/DNITest.php
+++ tests/DNITest.php
@@ -3,19 +3,24 @@
 use App\DNI;
 use PHPUnit\Framework\TestCase;
 
-use function PHPUnit\Framework\assertFalse;
+use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertTrue;
 
 Class DNITest extends TestCase{
 
     public function testlongitudDNI():void{
         $dni = new DNI();
-        assertTrue($dni->comprobarValidez("0000000A"), "La longuitud del DNI no es válida");
+        assertTrue($dni->comprobarValidez("00000000A"), "La longuitud del DNI no es válida");
     }
 
     public function testlongitudDNIMenor9():void{
-        $dni = new DNI();
-        assertFalse($dni->comprobarValidez("000000A"), "La longuitud del DNI es válida");
+        try{
+            $dni = new DNI();
+            $dni->comprobarValidez("000000A");
+        }
+        catch(Exception $e){
+            assertEquals("La longitud del DNI no es válida", $e->getMessage(), "No se ha lanzado la excepción correcta");
+        }
     }
 }
 ?>
